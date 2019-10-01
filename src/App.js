6660ecfe7d14e6ba68aafa6695a21d7e445e51cb
Your Name <you@example.com>
2019-09-30 20:30:45 -0500

    task completed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TaskForm from './components/tasks/taskForms/taskForms';
+
+
+function App() {
+  return (
+    <div>
+      <TaskForm/>
+    </div>
+  );
+}
+
+export default App;
